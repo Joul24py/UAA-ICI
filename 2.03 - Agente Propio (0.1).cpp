43478a997a65c4449fcdf922225de3be2127e8e0
Joul18 <54967754+Joul18@users.noreply.github.com>
2019-09-30 00:50:09 -0500

    Add files via upload

---
+++ 2.03 - Agente Propio (0.1).cpp
@@ -0,0 +1,41 @@
+/*
+UNIVERSIDAD AUTÓNOMA DE AGUASCALIENTES
+
+Centro de Ciencias Básicas
+Departamento de Ciencas de la Computación
+Inteligencia Artificial
+3° "A"
+
+Proyecto/Examen: Evolución de un Agente Inteligente para Aprender a Pasar un Laberinto Específico
+Anexo 2.03: Agente Propio (0.1)
+
+Profesor: Miguel Ángel Meza de Luna
+
+Alumnos:
+	Espinoza Sánchez Joel Alejandro
+	Reyes González Andrés Eleazar
+	Ortíz Nájera Alan Daniel
+
+Aguascalientes, Ags. 30 de septiembre de 2019
+*/
+
+#include <stdio.h>
+
+main()
+{
+	printf("    0.000            0.000            0.000     \n");
+	printf("0.000   0.000    0.000   0.000    0.000   0.000 \n");
+	printf("    0.000            0.000            0.000     \n");
+	printf("\n");
+	printf("    0.000                             0.000     \n");
+	printf("0.000   0.000                     0.000   0.000 \n");
+	printf("    0.000                             0.000     \n");
+	printf("\n");
+	printf("    0.000            0.000            0.000     \n");
+	printf("0.000   0.000    0.000   0.000    0.000   0.000 \n");
+	printf("    0.000            0.000            0.000     \n");
+	printf("\n");
+	printf("    0.000           -1.000            1.000     \n");
+	printf("0.000   0.000   -1.000  -1.000    1.000   1.000 \n");
+	printf("    0.000           -1.000            1.000     \n");
+}
