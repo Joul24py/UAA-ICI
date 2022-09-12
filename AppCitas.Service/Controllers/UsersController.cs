de0107f9eee8812ceb99025b1c0990a85ee09a52
Joul Espinoza Sánchez <alex.esa18@gmail.com>
2022-09-12 07:12:34 -0500

    Clase del jueves 8 de septiembre

--- AppCitas.Service/Controllers/UsersController.cs
+++ AppCitas.Service/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using AppCitas.Service.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using SQLitePCL;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppCitas.Service.Controllers;
 
@@ -19,15 +20,15 @@ public class UsersController : ControllerBase
 
     // GET api/users
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        return _context.Users.ToList();
+        return await _context.Users.ToListAsync();
     }
 
     // GET api/users/:id
     [HttpGet("{id}")]
-    public ActionResult<AppUser> GetUserById(int id)
+    public async Task<ActionResult<AppUser>> GetUserById(int id)
     {
-        return _context.Users.Find(id);
+        return await _context.Users.FindAsync (id);
     }
 }
